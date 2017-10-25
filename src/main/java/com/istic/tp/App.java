41bdfcd74699fd6e7bcfeaf9a3c1605eb65f1878
aferey <kwodhan@gmail.com>
2017-10-25 10:04:49 +0200

    launch junit test on target project

--- src/main/java/com/istic/tp/App.java
+++ src/main/java/com/istic/tp/App.java
@@ -1,13 +1,34 @@
 package com.istic.tp;
 
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws Throwable {
+        JUnitCore jUnitCore = new JUnitCore();
+
+        URL classUrl = new URL("file:///home/aferey/Documents/VV/MockMathSoftware/target/test-classes/");
+        URL classUrlsrc = new URL("file:///home/aferey/Documents/VV/MockMathSoftware/target/classes/");
+        URL[] classUrls = { classUrl,classUrlsrc };
+        URLClassLoader url = new URLClassLoader(classUrls);
+        Class simpleClass = url.loadClass("fr.istic.vv.AfeTest");
+        Result result = jUnitCore.run(simpleClass);
+        for (Failure f : result.getFailures()){
+
+            System.out.println(f.getException());
+            System.out.println(f.getMessage());
+        }
+
+
+
     }
 }
