9175a8d0e3d9605e6ef276d936f715e35fb7bcaf
Emmanuel Loisance <emmanuel.loisance@gmail.com>
2017-11-22 09:21:38 +0100

    test mutants created by MutatorComparison

---
+++ src/test/java/com/istic/tp/TestMutatorComparison.java
@@ -0,0 +1,31 @@
+package com.istic.tp;
+
+import com.istic.tp.mutant.Mutant;
+import com.istic.tp.mutator.*;
+import com.istic.tp.target.ProjectTarget;
+import javassist.CtMethod;
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestMutatorComparison extends TestCase {
+
+    public void testComparison() {
+        ProjectTarget projectTarget = new ProjectTarget(".");
+        CtMethod ctMethod = projectTarget.getMethod("com.istic.tp.mock.MockComparison", "aEstPlusGrandQueB");
+        assertNotNull(ctMethod);
+
+        List<Mutator> mutators = new ArrayList<Mutator>();
+        mutators.add(new ComparisonMutator());
+        assertEquals(1, mutators.size());
+
+        Scanner scanner = new Scanner(mutators);
+        List<Mutant> mutants = scanner.scan(ctMethod);
+        assertEquals(1, mutants.size());
+
+        Mutant mutant = mutants.get(0);
+        assertEquals(mutant.getInitial().getName(), mutant.getCtMethod().getName());
+    }
+
+}
---
+++ src/test/java/com/istic/tp/mock/MockComparison.java
@@ -0,0 +1,13 @@
+package com.istic.tp.mock;
+
+public class MockComparison {
+
+    public boolean aEstPlusGrandQueB(int a, int b) {
+        return a > b;
+    }
+
+    public boolean aEstPlusGrandOuEgalQueB(int a, int b) {
+        return a >= b;
+    }
+
+}
