5b88a19f63591be731fe38a63e0d13f69620d6c4
Emmanuel Loisance <emmanuel.loisance@gmail.com>
2017-11-22 12:05:42 +0100

    Fix TestMutatorComparison

--- src/test/java/com/istic/tp/TestMutatorComparison.java
+++ src/test/java/com/istic/tp/TestMutatorComparison.java
@@ -1,10 +1,12 @@
 package com.istic.tp;
 
 import com.istic.tp.mutant.Mutant;
+import com.istic.tp.mutator.ArithMutator;
 import com.istic.tp.mutator.ComparisonMutator;
 import com.istic.tp.mutator.Mutator;
 import com.istic.tp.target.ProjectTarget;
 import javassist.CtMethod;
+import javassist.bytecode.CodeIterator;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -16,15 +18,30 @@ import java.util.List;
 
 import static junit.framework.TestCase.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(Parameterized.class)
 public class TestMutatorComparison {
 
+    // /!\ important
+    // --> in bytecode, logic is inverse
+    // bytecode test if value is not opposite of operator
+    // ----------------
+    // for operator '<' if '>=' IS OK --> result KO
+    // for operator '>' if '<=' IS OK --> result KO
+    // for operator '<=' if '>' IS OK --> result KO
+    // for operator '>=' if '<' IS OK --> result KO
+    // ----------------
+    // so in parameters (initBytecode and mutateBytecode),
+    // added inverse bytecode of real operator.
+
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][] {
-                {"isGreater", 1, 2}, {"isLess", 1, 2},
-                {"isGreaterOrEqual", 1, 2}, {"isLessOrEqual", 1, 2}
+                { "isGreater", 1, 2, 164, 161 },
+                { "isLess", 1, 2, 162, 163 },
+                { "isGreaterOrEqual", 1, 2, 161, 164 },
+                { "isLessOrEqual", 1, 2, 163, 162 }
         });
     }
 
@@ -37,8 +54,14 @@ public class TestMutatorComparison {
     @Parameterized.Parameter(2)
     public int indexPosition;
 
+    @Parameterized.Parameter(3)
+    public int initBytecode;
+
+    @Parameterized.Parameter(4)
+    public int mutateBytecode;
+
     @Test
-    public void testComparison() {
+    public void testMutant() {
         ProjectTarget projectTarget = new ProjectTarget(".");
         CtMethod ctMethod = projectTarget.getMethod("com.istic.tp.mock.MockComparison", nameMethod);
         assertNotNull(ctMethod);
@@ -55,6 +78,40 @@ public class TestMutatorComparison {
         assertEquals(mutant.getInitial().getName(), mutant.getCtMethod().getName());
 
         assertEquals(indexPosition, mutant.getIndex().intValue());
+    }
+
+    @Test
+    public void testDoMutate() {
+        ProjectTarget projectTarget = new ProjectTarget(".");
+        CtMethod ctMethod = projectTarget.getMethod("com.istic.tp.mock.MockComparison", nameMethod);
+        assertNotNull(ctMethod);
+
+        List<Mutator> mutators = new ArrayList<Mutator>();
+        mutators.add(new ComparisonMutator());
+        assertEquals(1, mutators.size());
+
+        Scanner scanner = new Scanner(mutators);
+        List<Mutant> mutants = scanner.scan(ctMethod);
+
+        assertEquals(numberMutant, mutants.size());
+
+        CodeIterator ci = ctMethod.getMethodInfo().getCodeAttribute().iterator();
+
+        assertEquals(initBytecode, ci.byteAt(indexPosition));
+
+        for (Mutant mutant : mutants) {
+            mutant.doMutate();
+        }
+
+        ci = ctMethod.getMethodInfo().getCodeAttribute().iterator();
+        assertEquals(mutateBytecode, ci.byteAt(indexPosition));
+
+        for (Mutant mutant : mutants) {
+            mutant.revert();
+        }
+
+        ci = ctMethod.getMethodInfo().getCodeAttribute().iterator();
+        assertEquals(initBytecode, ci.byteAt(indexPosition));
     }
 
 }
